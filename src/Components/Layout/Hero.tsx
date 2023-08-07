4e9c2107558f910acd72c25c7e7d55ad8b8c8b9d
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-08-07 09:42:56 -0300

    feat: create component Hero

---
+++ src/Components/Layout/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import banner from '../../assets/img/capa.png';
+
+function Hero(props) {
+  return (
+    <div>
+      <h1>hero</h1>
+      <img src={banner} alt="a oman and man wearing a sunglasses" />
+    </div>
+  );
+}
+
+export default Hero;
